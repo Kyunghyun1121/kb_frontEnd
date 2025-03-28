534ac76a44b3e81292eab6ab6817f88d1ef26f80
경현 <jake.roh2000@gmail.com>
2025-03-28 16:31:49 +0900

    vue 404, 내비게이션 가드

---
+++ Vue.js/task-project/20250328/심화/router-test/src/pages/MemberInfo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card card-body">
+    <img :src="member.photo" class="img" />
+    <h4 class="mt-2">{{ member.name }}({{ member.role }})</h4>
+    <br />
+    <p>{{ member.desc }}</p>
+    <a
+      v-if="member.insta && member.insta !== ''"
+      class="fa fa-instagram m-1"
+      :href="member.insta"
+    ></a>
+    <a
+      v-if="member.facebook && member.facebook !== ''"
+      class="fa fa-facebook m-1"
+      :href="member.facebook"
+    ></a>
+    <a
+      v-if="member.youtube && member.youtube !== ''"
+      class="fa fa-youtube m-1"
+      :href="member.youtube"
+    ></a>
+    <br /><br />
+    <router-link to="/members">멤버 목록으로</router-link>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+import members from '@/members.json';
+
+const route = useRoute();
+const id = parseInt(route.params.id, 10);
+const member = members.find((m) => m.id == id);
+</script>
+
+<!-- "id": 2,
+"name": "이철희",
+"role": "드럼",
+"photo": "http://sstatic.naver.net/people/25/202004271203269741.jpg",
+"desc": "드럼연주가",
+"insta": "https://www.instagram.com/chulheedrum/",
+"facebook": "",
+"youtube": "https://www.youtube.com/channel/UCt4_srP6q_aPsmuZ3W3R-Uw" -->
